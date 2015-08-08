5bd36206627eedb7ecf29edc7439d178d4f4d161
Gabriel Barcia <gabriel.barcia@gmail.com>
2015-08-08 19:17:37 -0300

    3 edit functionalities working: Up, down and delete. Edit yest not started.

--- public/app/components/form/formDirective.js
+++ public/app/components/form/formDirective.js
@@ -1,3 +1,4 @@
+/// <reference path="../../../../typings/angularjs/angular.d.ts"/>
 angular.module('changeApp')
 .directive('renderForm', function (FormServ) {
     return {
@@ -16,10 +17,54 @@ angular.module('changeApp')
             componentData: '=component',
             type: '=type'
         },
-		templateUrl: "/app/components/form/renderComponentView.html"
+		templateUrl: "/app/components/form/renderComponentView.html",
+        controller: function ($scope, FormServ) {
+            
+            var deletion = false;
+            
+            $scope.confirmDeletion = function () {
+                return deletion;
+            };
+            
+            $scope.clickUpButton = function ( index ) {
+                // log only for reference. Need to be removed
+                console.log('Up button pressed for ' + index);
+                FormServ.currentFormData.moveUp(index);
+            };
+            
+            $scope.clickDownButton = function ( index ) {
+                // log only for reference. Need to be removed 
+                console.log('Down button pressed');
+                FormServ.currentFormData.moveDown(index);
+            };
+            
+            $scope.clickEditButton = function ( index ) {
+                // log only for reference. Need to be removed
+                console.log('Edit button pressed');
+            };
+            
+            $scope.clickDeleteButton = function ( index ) {
+                // log only for reference. Need to be removed
+                console.log('Delete button pressed');
+                deletion = true;
+                //code to show the confirmation dialog
+            };
+            
+            $scope.deletionConfirmed = function ( index ) {
+                //call deletion method in currentForm
+                console.log('Delete item with index: ', index);
+                FormServ.currentFormData.deleteItem(index);
+            };
+            
+            $scope.deletionCancelled = function () {
+                //close the confirmation dialog
+                deletion = false;
+            };                        
+            
+        }
     };
 })
-	.directive('createFormComponent', function (FormServ) {
+.directive('createFormComponent', function (FormServ) {
     return {
 		restrict: 'E',
 		link: function (scope, elem, attrs) {
@@ -27,16 +72,5 @@ angular.module('changeApp')
         },
 		templateUrl: "/app/components/form/createComponentView.html"
 
-    };
-})
-// REMOVE THIS DIRECTIVE - ONLY FOR DEBUG 
-.directive('peekFormDirective', function (FormServ) {
-    return {
-        scope: {},
-		template: "<pre>{{formData}}</pre>",
-		restrict: 'E',
-		link: function (scope, elem, attrs) {
-            scope.formData = FormServ.currentFormData.getAllItems();
-        }
     };
 });
--- public/app/components/form/formService.js
+++ public/app/components/form/formService.js
@@ -29,14 +29,37 @@ angular.module('changeApp')
     };
 
     Template.prototype.getItem = function (index) {
-        //retrieves a item on the repository and return it as json
+        //retrieves an item on the repository and return it
         return this.items[index];
+    };
+    
+    Template.prototype.setItem = function (index , item) {
+        // modify the item (index) on the repository and return it
+        return this.items.splice( index, 0, item );
+    };
+    
+    Template.prototype.deleteItem = function (index) {
+        // delete the item (index) on the repository and fix the order of the coming items
+        
+        console.log('index on factory: ', index);
+        
+        // deleting the item
+        this.items.splice( index, 1 );
+        
+        // correcting this.array.order for the other items after the deleted one 
+        for (var i = index; i < this.itemCount; i++) {
+            this.items[i].order--;
+        }
+        
+        // sort the array
+        this.sortItems();
+        return this;
     };
 
     Template.prototype.itemCount = function () {
         //return the numeber of items on the items collection inside template
         return this.items.length;
-    }
+    };
 
     Template.prototype.CreateTemplateItem = function (itemType, data) {
         return {
@@ -45,6 +68,64 @@ angular.module('changeApp')
             data  : data || {}
         };
     };
+
+    Template.prototype.sortItems = function () {
+        //return the numeber of items on the items collection inside template
+        return this.items.sort( function (a, b) {
+            var x = a.order; 
+            var y = b.order;
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    };
+
+
+    Template.prototype.moveUp = function ( index ) {
+        // Move the component defined on Index one position up
+        
+        // index cannot be less than zero 
+        if (index > 0) { 
+            // keep a reference variable 
+            var positionSwap = this.items[index].order;
+            // Change order of current item on index position then
+            // using the reference variable change the previous element
+            // I made it this way to avoid any problems in case that because of some
+            // logic issue, order get mess up, so in this way I always guarrante that
+            // order are swap
+            // I am not happy on not using the setItem method instead of doing it so
+            // manually, so I need to dig into that  later
+            this.items[index].order = this.items[index-1].order;
+            this.items[index-1].order = positionSwap;
+            
+            // sort the variable by Order
+            this.sortItems();
+        } 
+    };
+
+
+    Template.prototype.moveDown = function ( index ) {
+        // Move the component defined on Index one position down
+
+        // index starts from zero becaue it is an array $index and, on the other side  
+        // itemCount starts from 1, so I need to substract 1 to match items to array
+        if ( index < (this.itemCount() - 1) ) { 
+            // keep a reference variable 
+            var positionSwap = this.items[index].order;
+            // Change order of current item on index position then
+            // using the reference variable change the previous element
+            // I made it this way to avoid any problems in case that because of some
+            // logic issue, order get mess up, so in this way I always guarrante that
+            // order are swap
+            // I am not happy on not using the setItem method instead of doing it so
+            // manually, so I need to dig into that  later
+            this.items[index].order = this.items[index+1].order;
+            this.items[index+1].order = positionSwap;
+            
+            // sort the variable by Order
+            this.sortItems();
+        } 
+    };
+
+
 
     return {
         newItemData: newItemData,
